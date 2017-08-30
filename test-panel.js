ecbe3aea1e3d922e2640df63074b982251609836
Litvinov-Victor <nikki.solution@gmail.com>
2017-08-30 17:20:59 +0500

    fix

--- test-panel.js
+++ test-panel.js
@@ -80,8 +80,8 @@ $(function(){
 			    display: flex;\
 			    align-items: center;\
 			    justify-content: center;\
-				width: 50px;\
-				height: 50px;\
+				width: 25px;\
+				height: 30px;\
 				position: absolute !important;\
 				right: 0;\
 				top: 0;\
@@ -134,7 +134,7 @@ $(function(){
 
 	var cloneModBlock = $('\
 		<div class="test-panel__mod-btn">\
-			<svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">\
+			<svg fill="#FFFFFF" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">\
 			    <path d="M0 0h24v24H0z" fill="none"/>\
 			    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>\
 			</svg>\
